e867e18d89aca0de18f57eb493b533dacf598c94
zannatreya <zannatulbintabahar@yahoo.com>
2022-02-26 21:34:02 +0600

    dog api done

--- js/index.js
+++ js/index.js
@@ -7,5 +7,23 @@ const loadDog = () => {
 }
 
 const displayDog = (data) => {
-    console.log(data);
+    const divContainer = document.getElementById('main');
+    const first10Data = data.slice(0, 10);
+    for (const dog of first10Data) {
+        const div = document.createElement('div');
+        div.className = "col-lg-4"
+        console.log(dog);
+
+        div.innerHTML = `
+        <h2>${dog.name}</h2>
+        <p>${dog.temperament}</p>
+        <h4>${dog.weight.imperial}</h4>
+        <h4>${dog.life_span}</h4>
+        <img width="400px" height="250px" src="${dog.image.url}"/>
+        `;
+        console.log(div)
+
+        divContainer.appendChild(div);
+    }
+
 }
