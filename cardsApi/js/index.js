8cb0f6188b80fe9993800eee83fb836867c5b4f7
zannatreya <zannatulbintabahar@yahoo.com>
2022-02-27 21:38:18 +0600

    card details done

--- cardsApi/js/index.js
+++ cardsApi/js/index.js
@@ -55,6 +55,24 @@ const cardDisplay = (cards) => {
     }
 }
 
-const cardDetails = () => {
-
+const cardDetails = (code) => {
+    console.log(code);
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+        .then(res => res.json())
+        .then(data => {
+            const allCards = data.cards;
+            const singleCard = allCards.find(card => card.code === code)
+            const div = document.createElement('div');
+            main.innerHTML = '';
+            div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                    <img src="${singleCard.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${singleCard.suit}</h5>
+                         <p class="card-text">${singleCard.code}</p>
+                         <p class="card-text">${singleCard.value}</p>
+                    </div>
+                </div>`;
+            main.appendChild(div);
+        })
 }
